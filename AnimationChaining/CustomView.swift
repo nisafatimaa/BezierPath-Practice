a93fd5ac3957553a10092d61cd5b46e2439e2058
Nisa Fatima <nisafatima0000@gmail.com>
2024-07-16 21:01:25 +0500

    complete

---
+++ AnimationChaining/CustomView.swift
@@ -0,0 +1,14 @@
+//
+//  CustomView.swift
+//  AnimationChaining
+//
+//  Created by Macbook Pro on 04/04/2024.
+//
+
+import UIKit
+
+class CustomView: UIView {
+    
+
+        
+    }
--- AnimationChaining/ViewController.swift
+++ AnimationChaining/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+        let topPath : UIBezierPath = {
+            let path = UIBezierPath()
+            let rect = UIScreen.main.bounds
+            path.move(to: .zero)
+            path.addLine(to: CGPoint(x: rect.width, y: 0))
+            path.addLine(to: CGPoint(x: rect.width, y: rect.height * 1.5/5))
+            path.addCurve(to: CGPoint(x: 40, y: 75),
+                          controlPoint1: CGPoint(x: rect.width - 40, y: rect.height * 0.6 / 5),
+                          controlPoint2: CGPoint(x: rect.width - 70, y: rect.height * 0.4 / 5))
+            path.addCurve(to: CGPoint(x: 0, y: 50),
+                          controlPoint1: CGPoint(x: 10, y: 75 ),
+                          controlPoint2: CGPoint(x: 5, y: 65))
+            path.addLine(to: .zero)
+            path.close()
+            return path
+        }()
 
+    let shapeLayer : CAShapeLayer = {
+        let shape = CAShapeLayer()
+        shape.strokeColor = UIColor.brown.cgColor
+        shape.lineWidth = 5
+        return shape
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        shapeLayer.path = topPath.cgPath
+        view.layer.addSublayer(shapeLayer)
     }
 
-
 }
-
